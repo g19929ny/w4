538cfe51fc822427cc2c1f37add0f8c47488b6e8
g19929ny <95006261+g19929ny@users.noreply.github.com>
2021-12-14 04:22:30 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
+const storyText = 'その日は華氏86度であったため、とても暑かった。 だから:insertx:は:inserty:に行った。 着いたらまず足から:inserty:に入ってみた。とても冷たかった。しばらく遊んでいると、天気が:insertz:になってきた。体重が122ポンドの:insertx:は興奮していたが、津田はそうでもなかった。:inserty:では天気が変化するのは当たり前だったから。';
+const insertX = ['クマ', 'ネコ', 'ウサギ'];
+const insertY = ['海', 'プール', '川'];
+const insertZ = ['晴れ', '曇り', '雨'];
+
+randomize.addEventListener('click', result);
+
+function result() {
+  let newStory = storyText;
+
+  const xItem = randomValueFromArray(insertX);
+  const yItem = randomValueFromArray(insertY);
+  const zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replace(':insertx:',xItem);
+  newStory = newStory.replace(':insertx:',xItem);
+  newStory = newStory.replace(':inserty:',yItem);
+  newStory = newStory.replace(':inserty:',yItem);
+  newStory = newStory.replace(':inserty:',yItem);
+  newStory = newStory.replace(':insertz:',zItem);
+
+  if (customName.value !== '') {
+    const name = customName.value;
+    newStory = newStory.replace('津田', name);
+  }
+
+  if (document.getElementById("日本").checked) {
+    const weight = `${Math.round(122*(1/2.2))}kg`;
+    const temperature =  `摂氏${Math.round((86-32) * 5 / 9)}度`;
+    newStory = newStory.replace('華氏86度', temperature);
+    newStory = newStory.replace('122ポンド', weight);
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
